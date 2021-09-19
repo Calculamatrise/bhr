5c42bf8fce978dfcb70f9f6fbcb4b2f961e87e2e
Calculus <51772836+Calculamatrise@users.noreply.github.com>
2021-09-18 23:30:58 -0700

    Delete variable directory

--- variable/var.js
+++
@@ -1,8 +0,0 @@
-import Vector from "../class/Vector.js";
-
-export var records = [{}, {}, {}, {}, {}],
-    Hb = !1,
-    Lb = new Vector(40,50),
-    Mb = new Vector(-40,10),
-    charCount = document.getElementById("charcount"),
-    code = document.getElementById("trackcode");
