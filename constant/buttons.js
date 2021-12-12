e50fc1c35ee66037ad68433b8e3de5fa734a81ab
Calculus <tahar.ayoob@gmail.com>
2021-12-11 20:28:17 -0800

    Delete constant directory

--- constant/buttons.js
+++
@@ -1,7 +0,0 @@
-export const
-    clear = document.getElementById("new"),
-    load = document.getElementById("load"),
-    save = document.getElementById("save"),
-    upload = document.getElementById("upload"),
-    code = document.getElementById("trackcode"),
-    charCount = document.getElementById("charcount");
